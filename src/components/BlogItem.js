4f10c9cb1d5bf562e2b08d0c712e57521d8dc665
rdak <seva@chebykin.com>
2017-10-03 21:55:43 +0700

    change a bit for a commit

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -6,7 +6,8 @@ class BlogItem extends React.Component{
             <div className="blog-item">
                 <h3 className="blog-item__name">Blog Item</h2>
                 <p className="blog-item__description">
-                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, reprehenderit.</span><span>Earum nobis ad unde sequi, inventore, debitis doloremque! Sit, pariatur.</span><span>Voluptas, dolorum, magnam. Eius, sit officia dignissimos voluptatem. Fuga, saepe.</span><span>Facere praesentium, porro perspiciatis modi quas molestias natus deserunt voluptates.</span><span>Aperiam velit, expedita asperiores doloremque deserunt saepe, deleniti laborum labore.</span><span>Nisi blanditiis dolorum assumenda ut aut eum obcaecati rem laborum.</span><span>Ut ducimus, dolorem quam vero amet, enim fugit laborum similique.</span><span>Dolor facere nihil sequi odit unde aspernatur molestiae sed dolorem!</span><span>Nulla omnis repellat tenetur. Incidunt, accusamus vero repudiandae dolores at.</span><span>Ad, consequatur consectetur voluptates, nostrum dolorem illum quisquam asperiores sequi?</span>
+                    <span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, reprehenderit.</span>
+                    <span>Earum nobis ad unde sequi, inventore, debitis doloremque! Sit, pariatur.</span>
                 </p>
             </div>
         )
